16e2f69a32546e0be65a3e0e69f9b046a49676bd
Scott Hatfield <contact@hatfieldscott.com>
2023-06-30 07:12:58 -0500

    Added localized text :speech_balloon:

--- lib/clocky_app.dart
+++ lib/clocky_app.dart
@@ -1,5 +1,7 @@
 import 'package:clocky_mc_clockface/screens/clock/clock_types_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 /// A wrapper for the entire app, containing the base [MaterialApp] widget.
 class ClockyApp extends StatelessWidget {
@@ -9,6 +11,15 @@ class ClockyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Clocky McClockface',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.pink,
--- lib/screens/clock/clock_types_view.dart
+++ lib/screens/clock/clock_types_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'clock_types_controller.dart';
 
@@ -11,7 +12,10 @@ class ClockTypesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO some other stuff in here
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).app_title),
+      ),
+    );
   }
 }
